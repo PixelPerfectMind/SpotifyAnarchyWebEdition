c5747d3c836df3f7d76cc03669c1e6e7ae669fc0
Jonas <jonas.praast@web.de>
2023-12-20 10:55:26 +0100

    UPDATE start page

--- Models/DefaultValues.cs
+++ Models/DefaultValues.cs
@@ -1,26 +1,28 @@
-﻿namespace SpotifyAnarchyWebEdition.Models {
+﻿namespace SpotifyAnarchyWebEdition.Models
+{
 
     /// <summary>
     /// Default values for the SpotifyApi like client id and client secret<br>
     /// Feel free to replace with your own</br>
     /// </summary>
-    public class DefaultValues {
+    public class DefaultValues
+    {
 
         // Replace with your SpotifyApi credentials
 
         /// <summary>
         /// Your SpotifyApi client id
         /// </summary>
-        internal readonly string ClientId = "ReplaceWithYourOwn";
+        internal readonly string ClientId = "3eac98750c8f4f4da6dbf4dc63d182b6";
 
         /// <summary>
         /// Your SpotifyApi client secret
         /// </summary>
-        internal readonly string ClientSecret = "ReplaceWithYourOwn";
+        internal readonly string ClientSecret = "c269c041a81542c0b33780048c187475";
 
         /// <summary>
         /// A base64 encoded string of your client id and client secret, which is used for basic authorization
         /// </summary>
-        internal readonly string basicAuthorization = "ReplaceWithYourOwn";
+        internal readonly string basicAuthorization = "M2VhYzk4NzUwYzhmNGY0ZGE2ZGJmNGRjNjNkMTgyYjY6YzI2OWMwNDFhODE1NDJjMGIzMzc4MDA0OGMxODc0NzU=";
     }
 }
