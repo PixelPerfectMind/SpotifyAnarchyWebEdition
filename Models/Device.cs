e99598605a4ef336dad992dab9e768fdc011580f
PixelPerfectMind <jonas.praast@web.de>
2024-01-08 08:54:59 +0100

    ADD devices list

---
+++ Models/Device.cs
@@ -0,0 +1,31 @@
+﻿namespace SpotifyAnarchyWebEdition.Models
+{
+    public class Device
+    {
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPrivateSession { get; set; }
+        public bool IsRestricted { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int VolumePercent { get; set; }
+        public bool SupportsVolume { get; set; }
+
+        public string GetIconURL()
+        {
+            switch (Type)
+            {
+                case "Computer":
+                    return "/Content/Images/DeviceTypes/computer.png";
+                case "Smartphone":
+                    return "/Content/Images/DeviceTypes/smartphone.png";
+                case "Speaker":
+                    return "/Content/Images/DeviceTypes/speaker.png";
+                case "TV":
+                    return "/Content/Images/DeviceTypes/television.png";
+                default:
+                    return "/Content/Images/unknown_item.png";
+            }
+        }
+    }
+}
